98d3444b55e07e84e9c13966a538cb36b2998328
Michael Scott <scott.michael.s385@gmail.com>
2026-08-24 18:06:50 -0600

    removed the password preveiw for creating a volunteer and allowed users that came from "invite user" email to reset password

--- app/reset-password/page.js
+++ app/reset-password/page.js
@@ -12,7 +12,7 @@ export default function ResetPasswordPage() {
 
   useEffect(() => {
     const { data: { subscription } } = supabase.auth.onAuthStateChange((event) => {
-      if (event === 'PASSWORD_RECOVERY') {
+      if (event === 'PASSWORD_RECOVERY' || event === 'SIGNED_IN') {
         setReady(true)
       }
     })
